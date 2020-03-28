3790d046f39eeb0fc4775863ba2c8a0a1ea36760
calixteman <calixte.denizet@gmail.com>
2020-03-28 18:14:48 +0100

    Fix main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use cpp_parser::args;
+use cpp_parser::defaults;
+use cpp_parser::lexer::preprocessor::context::DefaultContext;
+use cpp_parser::lexer::source;
 use cpp_parser::lexer::{Lexer, Token};
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
-    let file = Path::new(env!("CARGO_MANIFEST_DIR")).join("benches/basic/ascii.cpp");
-    let s = fs::read(&file).unwrap();
-    let mut lexer = Lexer::new(&s);
+    let file = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("benches/basic/ascii.cpp");
+    let source = source::get_source_mutex();
+    let opt = args::PreprocOptions {
+        def: defaults::get_defined(),
+        sys_paths: defaults::get_sys_paths(),
+        includes: Vec::new(),
+        current_dir: PathBuf::from("."),
+    };
+    let mut lexer = Lexer::<DefaultContext>::new_from_file(file.to_str().unwrap(), source, opt);
     loop {
         let tok = lexer.next();
         if tok == Token::Eof {
