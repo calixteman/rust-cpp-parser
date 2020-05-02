593306e2140004d6cc2f3b2f5db62a41c4d22e44
calixteman <calixte.denizet@gmail.com>
2020-05-02 16:19:36 +0200

    Add licences Apache/MIT

--- src/parser/declarations/decl1.rs
+++ src/parser/declarations/decl1.rs
@@ -1,3 +1,8 @@
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 use super::super::r#type::{self, BaseType, CVQualifier, Type};
 use super::array::{Array, ArrayParser};
 use super::function::{Function, FunctionParser};
--- src/parser/expression/expr.rs
+++ src/parser/expression/expr.rs
@@ -1,3 +1,8 @@
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 use super::list::{ListInitialization, ListInitializationParser};
 use super::operator::{BinaryOp, Operator, UnaryOp};
 use super::params::{Parameters, ParametersParser};
